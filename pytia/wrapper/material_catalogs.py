e49558abfec6253bd49d8750b4b7d304ed3a71a1
deloarts <philip.delorenzo+github@outlook.com>
2022-11-14 14:00:28 +0100

    Fix pylint C0206

--- pytia/wrapper/material_catalogs.py
+++ pytia/wrapper/material_catalogs.py
@@ -54,12 +54,13 @@ class PyMaterialCatalog:
     @property
     def materials_str(self) -> Dict[str, List[str]]:
         """Returns a dictionary mapping material family names to material objects."""
-        materials = {}
-        for family in self.materials:
-            materials[family.name] = []
-            for material in self.materials[family]:
-                materials[family.name].append(material.name)
-        return materials
+        values = {}
+        for family, materials in self.materials.items():
+            items: list = []
+            values[family.name] = items
+            for material in materials:
+                items.append(material.name)
+        return values
 
     def __repr__(self) -> str:
         """Returns the representation of the object."""
